a8417e89f3d31c29f8646a0503901b5c53277496
Syam Pillai <syam.s.pillai@gmail.com>
2026-01-23 13:37:24 +0530

    3.5.2

--- lib/src/so_base.dart
+++ lib/src/so_base.dart
@@ -235,7 +235,7 @@ class Client {
         return _error("Can't re-login. Reason: $status");
       }
       return await this.command(command, attributes, false);
-    } else if(!sessionRequired) {
+    } else if (!sessionRequired) {
       _session = r["session"];
     }
     r.remove("session");
